4f312c9a4af54bbec50ca377e044c3e4b37a5529
Sreyas <Sreyassreelal@gmail.com>
2019-01-07 11:53:51 +0000

    tidy up

--- src/internals.rs
+++ src/internals.rs
@@ -2,16 +2,16 @@ use bcrypt::{hash,verify};
 use std::sync::mpsc::channel;
 
 pub trait Internal {
-    fn listen_for_hash_requests(&mut self);
+	fn listen_for_hash_requests(&mut self);
 	fn listen_for_verify_requests(&mut self);
 }
 
 impl Internal for super::SampBcrypt {
-    fn listen_for_hash_requests(&mut self) {
-        let (hash_complete_send, hash_complete_receiver) = channel();
+	fn listen_for_hash_requests(&mut self) {
+		let (hash_complete_send, hash_complete_receiver) = channel();
 		let (hash_start_sender, hash_start_receiver) = channel();
-        
-        self.hash_complete_receiver = Some(hash_complete_receiver);
+		
+		self.hash_complete_receiver = Some(hash_complete_receiver);
 		self.hash_start_sender = Some(hash_start_sender);
 		
 		std::thread::spawn(move || {
@@ -27,10 +27,10 @@ impl Internal for super::SampBcrypt {
 				}
 			}	
 		});
-    }
+	}
 
-    fn listen_for_verify_requests(&mut self) {
-        let (verify_complete_send, verify_complete_receiver) = channel();
+	fn listen_for_verify_requests(&mut self) {
+		let (verify_complete_send, verify_complete_receiver) = channel();
 		let (verify_start_sender, verify_start_receiver) = channel();
 		
 		self.verify_complete_receiver = Some(verify_complete_receiver);
@@ -49,5 +49,5 @@ impl Internal for super::SampBcrypt {
 				}
 			}	
 		});
-    }
+	}
 }
