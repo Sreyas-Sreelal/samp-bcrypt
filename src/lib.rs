bd70c93ea07617b8c3d8a8bb340497ca540d4821
SyS <sreyassreelal@gmail.com>
2020-07-10 18:57:33 +0530

    run rust fmt

--- src/lib.rs
+++ src/lib.rs
@@ -8,32 +8,32 @@ use std::collections::LinkedList;
 use threadpool::ThreadPool;
 
 initialize_plugin!(
-	natives: [
-		SampBcrypt::bcrypt_hash,
-		SampBcrypt::bcrypt_get_hash,
-		SampBcrypt::bcrypt_verify,
-		SampBcrypt::bcrypt_set_thread_limit
-	],
-	{
-		samp::plugin::enable_process_tick();
-		let samp_logger = samp::plugin::logger()
-			.level(log::LevelFilter::Info);
+    natives: [
+        SampBcrypt::bcrypt_hash,
+        SampBcrypt::bcrypt_get_hash,
+        SampBcrypt::bcrypt_verify,
+        SampBcrypt::bcrypt_set_thread_limit
+    ],
+    {
+        samp::plugin::enable_process_tick();
+        let samp_logger = samp::plugin::logger()
+            .level(log::LevelFilter::Info);
 
-		let _ = fern::Dispatch::new()
-			.format(|callback, message, record| {
-				callback.finish(format_args!("[SampBcrypt] [{}]: {}", record.level().to_string().to_lowercase(), message))
-			})
-			.chain(samp_logger)
-			.apply();
+        let _ = fern::Dispatch::new()
+            .format(|callback, message, record| {
+                callback.finish(format_args!("[SampBcrypt] [{}]: {}", record.level().to_string().to_lowercase(), message))
+            })
+            .chain(samp_logger)
+            .apply();
 
-		SampBcrypt {
-			hashes: LinkedList::new(),
-			pool: ThreadPool::new(3),
-			amx_list:Vec::new(),
-			hash_receiver:None,
-			hash_sender:None,
-			verify_receiver:None,
-			verify_sender:None
-		}
-	}
+        SampBcrypt {
+            hashes: LinkedList::new(),
+            pool: ThreadPool::new(3),
+            amx_list:Vec::new(),
+            hash_receiver:None,
+            hash_sender:None,
+            verify_receiver:None,
+            verify_sender:None
+        }
+    }
 );
